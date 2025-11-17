4f8e22beba0c8177a175ae6796df3d2855d35532
oivatO-soD-sotnaS <otavio.santos.lima.tds.2022@gmail.com>
2025-11-17 20:48:10 -0300

    test(backend): Add log de requisição para cara requisição feita para o backend. Serve para debugar.

--- backend/api/src/App/Middleware/AddJsonResponseHeader.php
+++ backend/api/src/App/Middleware/AddJsonResponseHeader.php
@@ -11,7 +11,7 @@ use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
 class AddJsonResponseHeader{
   public function __invoke(Request $request, RequestHandler $handler): Response{
     $response = $handler->handle($request);
-  
+    error_log(sprintf('Request URL: %s', (string) $request->getUri()));
     return $response->withHeader("Content-Type","application/json");
   }
 }
